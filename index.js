1626f0f1b81132778da62f95438fa5034011eae5
golinelli-99 <78856838+golinelli-99@users.noreply.github.com>
2025-11-17 11:53:53 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import nodemailer from "nodemailer";
+
+const app = express();
+app.use(express.json());
+
+app.post("/send", async (req, res) => {
+  const { messaggio } = req.body;
+
+  try {
+    let transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS
+      }
+    });
+
+    await transporter.sendMail({
+      from: process.env.GMAIL_USER,
+      to: "email_destinatario@example.com", // <-- QUI METTEREMO la mail della persona X
+      subject: "Nuovo messaggio dal cliente",
+      text: messaggio
+    });
+
+    return res.json({ success: true });
+  } catch (e) {
+    console.error(e);
+    return res.status(500).json({ success: false, error: e.message });
+  }
+});
+
+app.listen(3000, () => console.log("Server ready"));
